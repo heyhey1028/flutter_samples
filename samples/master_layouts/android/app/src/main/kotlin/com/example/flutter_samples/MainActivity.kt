ba77714c9bf9af18cbe20f724b2774435daf82e8
heyhey1028 <shoheiogawa1028@gmail.com>
2022-12-01 10:39:36 +0900

    add layout samples

---
+++ samples/master_layouts/android/app/src/main/kotlin/com/example/flutter_samples/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.master_layouts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ samples/master_layouts/lib/main.dart
@@ -0,0 +1,58 @@
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+// import './firebase_options.dart';
+import './widgets/app_scaffold.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              // height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  colors: [
+                    Colors.blue[900]!,
+                    Colors.blue[600]!,
+                    Colors.blue[400]!,
+                  ],
+                ),
+              ),
+              child: const Center(child: Text('Welcome to master_layouts')),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
