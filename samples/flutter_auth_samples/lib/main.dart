98a85707ef033b3150ad3b09d56cc12414306a77
heyhey1028 <shoheiogawa1028@gmail.com>
2024-02-19 21:11:08 +0900

    chore: add dotenv

--- samples/flutter_auth_samples/lib/main.dart
+++ samples/flutter_auth_samples/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_samples/global/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: MainApp()));
 }
 
